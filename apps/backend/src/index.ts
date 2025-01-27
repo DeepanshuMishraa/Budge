e7d969ce2f9367ee317af1d967422ededf62d3a3
Deepanshu <d4deepanshu723@gmail.com>
2025-01-27 22:14:04 +0530

    Added Frontend and Backend boilerplate

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,15 @@
+import { Hono } from "hono"
+import { logger } from "hono/logger";
+
+
+const app = new Hono();
+app.use("*", logger());
+
+app.get("/", (c) => {
+  return c.json({
+    message: "working"
+  })
+})
+
+export default app;
+
