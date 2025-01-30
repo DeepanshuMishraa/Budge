c60f3d4e486ef91a794620a452007f10a5db64a5
Deepanshu <d4deepanshu723@gmail.com>
2025-01-31 00:03:22 +0530

    Added Carousel

--- apps/frontend/components/Landing.tsx
+++ apps/frontend/components/Landing.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Hero } from './Hero'
 import { IntegrationCard } from './Integrations'
+import CarouselComponent from './carousel'
 
 const Landing = () => {
   return (
     <>
       <Hero />
       <IntegrationCard />
+      <CarouselComponent />
     </>
   )
 }
